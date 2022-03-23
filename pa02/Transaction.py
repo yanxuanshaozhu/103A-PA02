c005e3a514ae382eb5743571c386b8bb1bd6df4b
yanxuanshaozhu <973493269@qq.com>
2022-03-23 10:30:17 -0400

    add transaction methods, modify tracker and category

--- pa02/Transaction.py
+++ pa02/Transaction.py
@@ -1,10 +1,30 @@
 import sqlite3
 import csv
+
+
+def data_to_dict(data):
+    return {'item #':data[0], 'amount':data[1], 'category':data[2], 'date': data[3], 'desc':data[4]}
+
+def data_to_list(data):
+    return [data_to_dict(row) for row in data]
+
 class Transaction:
+
+
     def __init__(self, filename):
         con = sqlite3.connect(filename)
         cur = con.cursor()
         cur.execute("DROP TABLE IF EXISTS transactions")
-        cur.execute("CREATE TABLE IF NOT EXISTS transactions (item_id int, amount int, category text, date text, description text)")
+        cur.execute("CREATE TABLE IF NOT EXISTS transactions (item int, amount int, category text, date text, description text)")
         con.commit()
         con.close()
+        self.db = filename
+
+    def select_all(self):
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+        cur.execute("select * from transactions")
+        rows = cur.fetchall()
+        conn.commit()
+        conn.close()
+        return data_to_list(rows)
