21b2c6b3ff15543915991565e99a8868c2297efb
Zeng-Lecheng <50188852+Zeng-Lecheng@users.noreply.github.com>
2022-03-23 21:52:01 -0400

    [Fix](test_transaction) Fix testcases

--- pa02/test_transaction.py
+++ pa02/test_transaction.py
@@ -14,19 +14,18 @@ def setup_and_teardown():
     db = Transaction('test.db')
     con = sqlite3.connect('test.db')
     cur = con.cursor()
-    cur.execute("DROP TABLE IF EXISTS transactions")
     cur.execute(
         "CREATE TABLE IF NOT EXISTS transactions (item int, amount int, category text, date text, description text)")
     cur.execute("INSERT into transactions (item, amount, category, date, description) values "
                 "(1, 2, 'test1', '2022-03-21', 'This is a test')")
     cur.execute("INSERT into transactions (item, amount, category, date, description) values "
-                "(1, 2, 'test1', '2022-03-21', 'This is test 1')")
+                "(2, 2, 'test1', '2022-03-21', 'This is test 1')")
     cur.execute("INSERT into transactions (item, amount, category, date, description) values "
-                "(1, 2, 'test2', '2022-03-21', 'This is a test 2')")
+                "(3, 3, 'test2', '2022-03-21', 'This is a test 2')")
     cur.execute("INSERT into transactions (item, amount, category, date, description) values "
-                "(1, 2, 'test2', '2022-03-22', 'This is a test 3')")
+                "(4, 3, 'test2', '2022-03-22', 'This is a test 3')")
     cur.execute("INSERT into transactions (item, amount, category, date, description) values "
-                "(1, 2, 'test2', '2022-03-22', 'This is a test 4')")
+                "(5, 4, 'test2', '2022-03-22', 'This is a test 4')")
     con.commit()
     con.close()
     yield
@@ -49,7 +48,11 @@ def test_select_all(setup_and_teardown):
     result = db.select_all()
     assert result == [
         {'item #': 1, 'amount': 2, 'category': 'test1', 'date': '2022-03-21', 'desc': 'This is a test'},
-        {'item #': 1, 'amount': 2, 'category': 'test1', 'date': '2022-03-21', 'desc': 'This is test 1'},
-        {'item #': 1, 'amount': 2, 'category': 'test2', 'date': '2022-03-21', 'desc': 'This is a test 2'},
-        {'item #': 1, 'amount': 2, 'category': 'test2', 'date': '2022-03-22', 'desc': 'This is a test 3'},
-        {'item #': 1, 'amount': 2, 'category': 'test2', 'date': '2022-03-22', 'desc': 'This is a test 4'}]
+        {'item #': 2, 'amount': 2, 'category': 'test1', 'date': '2022-03-21', 'desc': 'This is test 1'},
+        {'item #': 3, 'amount': 3, 'category': 'test2', 'date': '2022-03-21', 'desc': 'This is a test 2'},
+        {'item #': 4, 'amount': 3, 'category': 'test2', 'date': '2022-03-22', 'desc': 'This is a test 3'},
+        {'item #': 5, 'amount': 4, 'category': 'test2', 'date': '2022-03-22', 'desc': 'This is a test 4'}]
+
+
+def test_delete(setup_and_teardown):
+    pass
